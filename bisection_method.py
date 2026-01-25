4c52d33005c8e9c545111b36441c094c0a1c9c0e
stnandiath <ph25mscst11030@iith.ac.in>
2026-01-25 17:57:55 +0530

    Added bisection method file

--- bisection_method.py
+++ bisection_method.py
@@ -1,4 +1,17 @@
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-x=1
+def f(x):
+    return 2*np.cos(x) - x
+x = np.linspace(0, 7, 400)
+y = f(x)
+
+plt.figure()
+plt.plot(x, y)
+plt.axhline(0)      
+plt.xlabel("x")
+plt.ylabel("f(x)")
+plt.title("Plot of f(x) = 2 cos(x) - x")
+plt.grid(True)
+plt.show()
